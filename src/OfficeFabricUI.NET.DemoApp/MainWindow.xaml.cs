0bb1a679e493f23ec2831cf03b20249434ab5fee
Adolfo Marinucci <adospace@msn.com>
2019-12-09 14:35:39 +0100

    Adding FabricWindow

--- src/OfficeFabricUI.NET.DemoApp/MainWindow.xaml.cs
+++ src/OfficeFabricUI.NET.DemoApp/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace OfficeFabricUI.DemoApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FabricWindow
     {
         public MainWindow()
         {
---
+++ src/OfficeFabricUI/FabricWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace OfficeFabricUI
+{
+    public class FabricWindow : Window
+    {
+        static FabricWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FabricWindow), 
+                new FrameworkPropertyMetadata(typeof(FabricWindow)));
+        }
+
+
+    }
+}
