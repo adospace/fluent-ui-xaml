12bc5037da9395b2136f4caae9c041ebf682d30f
Ado Marinucci <ado@realtimeresearch.com>
2020-02-19 22:10:37 +0100

    Introducing ListBox style + compact mode

---
+++ src/OfficeFabricUI/FabricGridExtra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace OfficeFabricUI
+{
+    //public static class FabricGridExtra
+    //{
+    //    public static bool GetModalSelection(DependencyObject obj)
+    //    {
+    //        return (bool)obj.GetValue(ModalSelectionProperty);
+    //    }
+
+    //    public static void SetModalSelection(DependencyObject obj, bool value)
+    //    {
+    //        obj.SetValue(ModalSelectionProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty ModalSelectionProperty =
+    //        DependencyProperty.RegisterAttached("ModalSelection", typeof(bool), typeof(FabricGridExtra), new PropertyMetadata(false));
+
+    //}
+}
